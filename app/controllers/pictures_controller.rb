9e3237b5f15c12e37d39d3bacb973ef1465627fe
Dan Walsh <dwalsh2@chicagobooth.edu>
2017-07-20 00:54:23 +0000

    partial early branch

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,37 @@
 class PicturesController < ApplicationController
     def new_form
+        
         render("pic_templates/new_form.html.erb")
     end
     
-    def 
+    def index
+        
+        # our_photos=Photo.each do |pics|
+            
+        
+        render("pic_templates/index.html.erb")
+    end
     
     
+    def show
+        
+        @pic = Photo.find(params["the_id"])
+        # the_id_number = params["an_id"]
+        
+        # pic = Photo.find(the_id_number)
+        
+        # @the_source = pic.source
+        # @the_caption = pic.caption
+        
+        render("pic_templates/show.html.erb")
+    end
+    
+    # def create_row
+    #     p = Photo.new_form
+        
+    #     p.source = 
+    
+    # end
     
     
 end
